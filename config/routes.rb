75c4d182e09e1b57bd4911a64a7b01144a0350b3
Juan Bezanilla <juanbezanillaz@gmail.com>
2024-01-31 17:00:14 -0300

    CRUD listo

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.htm
+
+  # read all
   get "restaurants", to: "restaurants#index"
+  # Create
   get "restaurants/new", to: "restaurants#new"
   post "restaurants", to: "restaurants#create"
+  # read one
   get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  # update
   get "restaurants/:id/edit", to: "restaurants#edit"
   patch "restaurants/:id", to: "restaurants#update"
-
+  # Delete
+  delete "restaurants/:id", to: "restaurants#destroy"
 
   # Defines the root path route ("/")
   # root "articles#index"
